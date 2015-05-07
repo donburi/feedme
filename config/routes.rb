d749d3e6b018136dea2d93940984b6b478b12c32
Don Currie <d.currie@utoronto.ca>
2015-05-07 19:52:47 -0400

    dropped email column from User

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users
-  get 'home/index'
-
   root to: "home#index"
+  devise_for :users
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150507230903) do
+ActiveRecord::Schema.define(version: 20150507235148) do
 
   create_table "users", force: :cascade do |t|
     t.string   "first_name"
     t.string   "last_name"
-    t.string   "email"
     t.string   "type"
     t.boolean  "in_toronto?"
     t.datetime "created_at",  null: false
